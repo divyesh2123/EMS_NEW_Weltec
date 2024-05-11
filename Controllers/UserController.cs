abae7229d3a7123b0fa3af0db1c521b323b56ac7
lenovo <lenovo@LAPTOP-BLNTEBH7>
2024-05-11 10:17:05 +0530

    zc

---
+++ Controllers/UserController.cs
@@ -0,0 +1,23 @@
+﻿using EMS.BussinessService.Concreate;
+using EMS.BussinessService.Interface;
+using EMS_New.Repository.Concreate;
+using EMS_New.Repository.Interface;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EMS_NEW_Weltec.Controllers
+{
+    public class UserController : Controller
+    {
+        private readonly IUserService userService;
+        public UserController()
+        {
+            userService = new UserService();
+        }
+
+        public IActionResult Index()
+        {
+            var p = userService.GetUsers();
+            return View(p);
+        }
+    }
+}
