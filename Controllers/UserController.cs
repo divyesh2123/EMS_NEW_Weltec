75f7b564e7cc631879530ed7f27ec8470a3477d7
lenovo <lenovo@LAPTOP-BLNTEBH7>
2024-05-12 10:58:14 +0530

    sdasd

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -1,4 +1,5 @@
-﻿using EMS.BussinessService.Concreate;
+﻿using EMS.BussinessEnitiy;
+using EMS.BussinessService.Concreate;
 using EMS.BussinessService.Interface;
 using EMS_New.Repository.Concreate;
 using EMS_New.Repository.Interface;
@@ -14,10 +15,39 @@ namespace EMS_NEW_Weltec.Controllers
             userService = new UserService();
         }
 
+        public IActionResult AddUser()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult AddUser(UserViewModel userViewModel)
+        {
+            userService.AddUser(userViewModel); 
+            return RedirectToAction("Index");   
+        }
+
         public IActionResult Index()
         {
             var p = userService.GetUsers();
+
+            if(p.Count == 0)
+            {
+                ViewData["Message"] = "No Record found";
+                ViewBag.Message = "test";
+                string d= ViewBag.Message;
+                string d1 = Convert.ToString(ViewData["Message"]);
+            }
             return View(p);
+        }
+
+        public IActionResult DeleteInfo(int id)
+        {
+             var d = userService.DeleteUser(id);
+
+            TempData["message"] = "User Deleted suc...";
+
+           return RedirectToAction("Index");    
         }
     }
 }
